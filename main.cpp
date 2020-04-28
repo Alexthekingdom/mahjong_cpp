fbe049bc8743a8339823e32983afcc677d949ecc
Alex <38998070+Alexthekingdom@users.noreply.github.com>
2020-04-28 13:03:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ vector<string> hand;
 vector<string> shun_zi, ke_zi, gang_zi;
 
 int shu[28] = { 0 }, feng[5] = { 0 }, jian[4] = { 0 };//数牌按饼条万的1-9顺序排列，数组数字代表该牌的数量
+int shu_remain[28] = { 0 }, feng_remain[5] = { 0 }, jian_remain[4] = { 0 };//数牌按饼条万的1-9顺序排列，数组数字代表该牌的数量
 int shu_ting[28][6] = { 0 }, feng_ting[5] = { 0 }, jian_ting[4] = { 0};
 //数牌的6个数分别表示是否听该牌，是否能杠，是否能碰，是否左两位吃，是否右两位吃，是否左右吃
 
@@ -78,8 +79,47 @@ void canmingpai() {
     }
 }
 
+int shu_location(string s){ //返回数牌的位置
+    int num = s[1] - '0';
+    switch (s[0]) {
+        case 'B':
+            return num;
+        case 'T':
+            return num + 9;
+        case 'W':
+            return num + 18;
+        default:
+            return -1;
+    }
+}
+
+void del_remain(string stmp, int n){
+    int num = stmp[1] - '0';
+    switch (stmp[0]){
+        case 'F':
+            feng_remain[num] -= n;
+            break;
+        case 'J':
+            jian_remain[num] -= n;
+            break;
+        default:
+            shu_remain[shu_location(stmp)] -= n;
+    }
+}
+
 int main()
 {
+    for (int i = 1; i<28; i++) {
+        shu_remain[i] = 4;
+    }
+    for (int i = 1; i<5; i++) {
+        feng_remain[i] = 4;
+    }
+    for (int i = 1; i<4; i++) {
+        jian_remain[i] = 4;
+    }
+    
+    
     int turnID;
     string stmp;
 
@@ -116,34 +156,43 @@ int main()
             hand.push_back(stmp);
         }
         
-        // 之后的每轮，需要根据request判断自己手牌变化情况
+        // 之后的每轮，需要根据request判断自己手牌变化情况，以及当前场面剩余牌的形势
         for(int i = 2; i < turnID; i++) {
             sin.clear();
             sin.str(request[i]);
             sin >> itmp;
-            if(itmp == 2) {
+            if(itmp == 2) { //如果是自己摸牌
                 sin >> stmp;
                 hand.push_back(stmp);
+                del_remain(stmp, 1);
                 sin.clear();
             }
-            else if (itmp == 3) {
+            else if (itmp == 3) { // 如果是其他情况
                 int now_id;
                 sin >> now_id; //当前轮玩家id
-                if (now_id!=myPlayerID) {
-                    continue; // 如果当前轮id不是我，那不会影响我的手牌，故直接过
-                }
-                // 否则影响手牌，看下一步
                 string op;
                 sin >> op; //当前轮操作
+                
                 if (op == "PLAY") {
                     sin >> stmp;
-                    hand.erase(find(hand.begin(), hand.end(), stmp)); //如果我play了某牌，就从手里删掉
+                    if (now_id==myPlayerID) {
+                        hand.erase(find(hand.begin(), hand.end(), stmp)); //如果我play了某牌，就从手里删掉
+                    }
+                    //别人打出的牌，从剩余牌库里删掉
+                    else{
+                        del_remain(stmp, 1);
+                    }
                 }
                 else if (op == "PENG"){
                     sin >> stmp;
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
+                    if (now_id==myPlayerID) { // 先清除打出去的牌
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                    }
+                    else{
+                        del_remain(stmp, 1);
+                    }
                     sin.clear();
-                    //以下对PENG进行手牌变更
+                    
                     sin.str(request[i-1]);
                     sin >> itmp >> itmp >> stmp;
                     if (stmp!="CHI") {
@@ -152,40 +201,106 @@ int main()
                     else{
                         sin >> stmp;
                     }
-                    ke_zi.push_back(stmp);
-                    // PENG有两张，都从手里删掉
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
+                    
+                    //以下对PENG进行手牌变更
+                    if (now_id==myPlayerID) {
+                        ke_zi.push_back(stmp);
+                        // PENG有两张，都从手里删掉
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                    }
+                    //是其他人碰的牌，从剩余牌库删掉其亮出来的两张牌
+                    else{
+                        del_remain(stmp, 2);
+                    }
+                    
                 }
                 else if (op == "CHI"){
-                    sin >> stmp;
-                    shun_zi.push_back(stmp);
-                    sin >> stmp;
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
                     sin.clear();
-                }
-                else if (op == "GANG"){
-                    sin.clear();
-                    //以下对GANG进行手牌变更
                     sin.str(request[i-1]);
-                    sin >> itmp;
-                    if (itmp == 2) { // 如果上回合摸牌，则为暗杠
+                    sin >> itmp >> itmp >> stmp;
+                    if (stmp!="CHI") {
+                        sin >> stmp >> stmp;
+                    }
+                    else{
                         sin >> stmp;
                     }
-                    else { // 否则为明杠
-                        sin >> itmp >> stmp;
-                        if (stmp!="CHI") {
-                            sin >> stmp >> stmp;
-                        }
-                        else{
+                    
+                    if (now_id == myPlayerID) { //先加上吃的是哪张牌，后续再删掉或打出
+                        hand.push_back(stmp);
+                    }
+                    else {
+                        del_remain(stmp, -1);
+                    }
+                    
+                    sin.clear();
+                    sin.str(request[i]);
+                    sin >> itmp >> itmp >> stmp;
+                    if (now_id==myPlayerID) {
+                        sin >> stmp;
+                        shun_zi.push_back(stmp);
+                        
+                        string tmp = stmp; // 清理掉吃的三张牌
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                        tmp[1] = ((stmp[1]-'0')+1)+'0';
+                        hand.erase(find(hand.begin(), hand.end(), tmp));
+                        tmp[1] = ((stmp[1]-'0')-1)+'0';
+                        hand.erase(find(hand.begin(), hand.end(), tmp));
+                        
+                        sin >> stmp; // 打出另一张牌
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                    }
+                    else {
+                        sin >> stmp;
+                        del_remain(stmp, 1);
+                        string tmp = stmp;
+                        tmp[1] = ((stmp[1]-'0')+1)+'0';
+                        del_remain(tmp, 1);
+                        tmp[1] = ((stmp[1]-'0')-1)+'0';
+                        del_remain(tmp, 1);
+                        
+                        sin >> stmp; // 打出另一张牌
+                        del_remain(stmp, 1);
+                    }
+                }
+                else if (op == "GANG"){
+                    if (now_id==myPlayerID) {
+                        sin.clear();
+                        //以下对GANG进行手牌变更
+                        sin.str(request[i-1]);
+                        sin >> itmp;
+                        if (itmp == 2) { // 如果上回合摸牌，则为暗杠
                             sin >> stmp;
                         }
+                        else { // 否则为明杠
+                            sin >> itmp >> stmp;
+                            if (stmp!="CHI") {
+                                sin >> stmp >> stmp;
+                            }
+                            else{
+                                sin >> stmp;
+                            }
+                        }
+                        gang_zi.push_back(stmp);
+                        // GANG有3张，都从手里删掉
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
+                        hand.erase(find(hand.begin(), hand.end(), stmp));
                     }
-                    gang_zi.push_back(stmp);
-                    // GANG有3张，都从手里删掉
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
-                    hand.erase(find(hand.begin(), hand.end(), stmp));
+                    else {
+                        sin.clear();
+                        sin.str(request[i-1]);
+                        sin >> itmp >> itmp >> stmp;
+                        if (stmp != "DRAW") { //如果是DRAW，那我也不知道剩余牌库是否发生了变化
+                            if (stmp!="CHI") {
+                                sin >> stmp >> stmp;
+                            }
+                            else{
+                                sin >> stmp;
+                            }
+                            del_remain(stmp, 3); //杠会亮出来三张，所以牌库减3
+                        }
+                    }
                 }
             }
         }// 结束更新手牌
